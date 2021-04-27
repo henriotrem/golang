fd34af2ffd5ac697adb3bd5ddd854a702367ba69
remihenriot <henriot.rem@gmail.com>
2021-04-27 14:17:10 +0200

    Read me for RAFT protocol

--- project/next/main.go
+++ project/next/main.go
@@ -2,100 +2,222 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-type Action int
-
 const (
-	Start     Action = 0
-	Bootstrap Action = 1
-	Follow    Action = 2
-	Serve     Action = 3
-	Add       Action = 4
-	Get       Action = 5
-	Quit      Action = 6
+	Offline = 0
+	Online  = 1
 )
+
+type StartAction struct {
+	Contact chan Command
+}
+
+type BootstrapAction struct {
+}
+
+type FollowAction struct {
+	Contact chan Command
+	Action  interface{}
+}
+
+type ServeAction struct {
+	ID        string
+	Routing   [][]string
+	Addresses map[string]Address
+}
+
+type UpdateAction struct {
+	ID   string
+	Busy int
+}
+
+type QuitAction struct {
+}
 
 type Command struct {
 	Origin chan Command
-	Action Action
-	Value  interface{}
+	Action interface{}
 }
 
-func CreateCommand(origin chan Command, action Action) Command {
+type Address struct {
+	Busy  int
+	Input chan Command
+}
+
+func CreateCommand(origin chan Command, action interface{}) Command {
 	return Command{Origin: origin, Action: action}
 }
 
 type Server struct {
 	ID        string
 	Input     chan Command
+	State     int
+	Busy      int
 	Quit      chan bool
 	Bootstrap chan Command
 	Routing   [][]string
-	Servers   map[string]chan Command
+	Addresses map[string]Address
 }
 
 func CreateServer() *Server {
 
-	return &Server{Input: make(chan Command), Quit: make(chan bool), Servers: make(map[string]chan Command)}
+	return &Server{Input: make(chan Command), Quit: make(chan bool), Addresses: make(map[string]Address)}
 }
 
-func (server *Server) start(command Command) {
+func (server *Server) busiest() chan Command {
 
+	var max = server.Busy
+	var busiest chan Command
+
+	for _, address := range server.Addresses {
+
+		if server.Input != address.Input && address.Busy > max {
+			max = address.Busy
+			busiest = address.Input
+		}
+	}
+
+	return busiest
 }
 
-func (server *Server) bootstrap(command Command) {
+func (server *Server) start(origin chan Command, action StartAction) {
+	fmt.Println("Start", server.ID)
 
+	if action.Contact == nil {
+
+		server.ID = "#"
+		server.State = Online
+		server.Busy = rand.Intn(100)
+		server.Routing = append(server.Routing, []string{server.ID})
+		server.Addresses[server.ID] = Address{Busy: server.Busy, Input: server.Input}
+	} else {
+
+		command := CreateCommand(server.Input, BootstrapAction{})
+		action.Contact <- command
+	}
+}
+
+func (server *Server) bootstrap(origin chan Command, action BootstrapAction) {
+	fmt.Println("Bootstrap", server.ID)
+
+	var command Command
+
+	if busy := server.busiest(); busy == nil {
+
+		newServerID := server.ID + "2"
+		server.ID = server.ID + "1"
+
+		server.Addresses[newServerID] = Address{Busy: 0, Input: origin}
+		server.Addresses[server.ID] = Address{Busy: server.Busy, Input: server.Input}
+
+		server.Routing = append(server.Routing, []string{server.ID, newServerID})
+
+		command = CreateCommand(server.Input, ServeAction{ID: newServerID, Routing: server.Routing, Addresses: server.Addresses})
+	} else {
+
+		command = CreateCommand(server.Input, FollowAction{Contact: busy, Action: action})
+	}
+
+	origin <- command
+}
+
+func (server *Server) follow(origin chan Command, action FollowAction) {
+	fmt.Println("Follow", server.ID)
+
+	action.Contact <- CreateCommand(server.Input, action.Action)
+}
+
+func (server *Server) serve(origin chan Command, action ServeAction) {
+	fmt.Println("Serve", server.ID)
+
+	server.ID = action.ID
+	server.State = Online
+	server.Busy = rand.Intn(100)
+	server.Routing = action.Routing
+	server.Addresses = action.Addresses
+}
+
+func (server *Server) update(origin chan Command, action UpdateAction) {
+	fmt.Println("Update", server.ID)
+
+	server.Addresses[action.ID] = Address{Busy: action.Busy, Input: origin}
+}
+
+func (server *Server) ping() {
+	fmt.Println("Ping", server.ID)
+
+	channels := make(map[chan Command]bool)
+	channels[server.Input] = true
+
+	for _, address := range server.Addresses {
+
+		if !channels[address.Input] {
+
+			channels[address.Input] = true
+			address.Input <- Command{Origin: server.Input, Action: UpdateAction{ID: server.ID, Busy: server.Busy}}
+		}
+	}
 }
 
 func (server *Server) run() {
 
 	for {
-		command := <-server.Input
 
-		switch command.Action {
-		case Start:
-			fmt.Println("Start")
-			server.start(command)
-		case Bootstrap:
-			fmt.Println("Bootstrap")
-			server.bootstrap(command)
-		case Follow:
-			fmt.Println("Follow")
-		case Serve:
-			fmt.Println("Serve")
-		case Add:
-			fmt.Println("Add")
-		case Get:
-			fmt.Println("Get")
-		case Quit:
-			fmt.Println("Quit")
+		var pingCh <-chan time.Time
+
+		if server.State == Online {
+			pingCh = time.After(2 * time.Second)
 		}
+
+		select {
+		case command := <-server.Input:
+			switch action := command.Action.(type) {
+			case StartAction:
+				server.start(command.Origin, action)
+			case BootstrapAction:
+				server.bootstrap(command.Origin, action)
+			case FollowAction:
+				server.follow(command.Origin, action)
+			case ServeAction:
+				server.serve(command.Origin, action)
+			case UpdateAction:
+				server.update(command.Origin, action)
+			case QuitAction:
+				return
+			}
+		case <-pingCh:
+			server.ping()
+		}
+
 	}
 }
 
 func main() {
 
-	server1 := CreateServer()
-	go server1.bootstrap()
+	var servers []*Server
 
-	server2 := CreateServer()
-	go server2.bootstrap()
+	for i := 0; i < 10; i++ {
 
-	time.Sleep(3 * time.Second)
+		server := CreateServer()
+		go server.run()
 
-	server1.Quit <- true
-	server2.Quit <- true
+		if i == 0 {
+			server.Input <- CreateCommand(nil, StartAction{})
+		} else {
+			server.Input <- CreateCommand(nil, StartAction{Contact: servers[0].Input})
+
+		}
+
+		servers = append(servers, server)
+		time.Sleep(3 * time.Second)
+	}
+
+	for _, server := range servers {
+
+		server.Quit <- true
+		fmt.Println(server)
+	}
 }
-
-// if command.Args.bootstrap == nil {
-
-// 	server.ID = "#"
-// 	server.Routing = append(server.Routing, []string{server.ID})
-// 	server.Servers[server.ID] = server.Input
-// } else {
-
-// 	command := CreateCommand(server.Input, Join)
-// 	server.Bootstrap <- command
-// }
